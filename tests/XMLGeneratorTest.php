9dd399dd03b6983f1c6edc54a772269de7f6ac38
Zhelninskiy Daniil <webmailexec@gmail.com>
2018-07-27 18:32:32 +0300

    Fixed XMLGenerator test

--- tests/XMLGeneratorTest.php
+++ tests/XMLGeneratorTest.php
@@ -47,27 +47,27 @@ class XMLGeneratorTest extends \PHPUnit_Framework_TestCase
             ->setMpn('4687DF4')
             ->setChannel($channel);
 
-        $result = '<?xml version="1.0" encoding="UTF-8"?>
-<rss xmlns:g="http://base.google.com/ns/1.0" version="2.0">
-<channel>
-<title>TestFeed</title>
-<link>https://getcomposer.org/</link>
-<description>Test Feed Description</description>
-<item>
-<g:id>1</g:id>
-<g:title>Test Title 1</g:title>
-<g:description>Test Description 1</g:description>
-<g:link>https://www.google.com/</g:link>
-<g:image_link>https://getcomposer.org/img/logo-composer-transparent5.png</g:image_link>
-<g:condition>new</g:condition>
-<g:availability>in stock</g:availability>
-<g:price>100 RUB</g:price>
-<g:brand>Test</g:brand>
-<g:mpn>4687DF4</g:mpn>
-</item>
-</channel>
-</rss>
-';
+        $result = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+        $result .= '<rss xmlns:g="http://base.google.com/ns/1.0" version="2.0">' . PHP_EOL;
+        $result .= '<channel>' . PHP_EOL;
+        $result .= '<title>TestFeed</title>' . PHP_EOL;
+        $result .= '<link>https://getcomposer.org/</link>' . PHP_EOL;
+        $result .= '<description>Test Feed Description</description>' . PHP_EOL;
+        $result .= '<item>' . PHP_EOL;
+        $result .= '<g:id>1</g:id>' . PHP_EOL;
+        $result .= '<g:title>Test Title 1</g:title>' . PHP_EOL;
+        $result .= '<g:description>Test Description 1</g:description>' . PHP_EOL;
+        $result .= '<g:link>https://www.google.com/</g:link>' . PHP_EOL;
+        $result .= '<g:image_link>https://getcomposer.org/img/logo-composer-transparent5.png</g:image_link>' . PHP_EOL;
+        $result .= '<g:condition>new</g:condition>' . PHP_EOL;
+        $result .= '<g:availability>in stock</g:availability>' . PHP_EOL;
+        $result .= '<g:price>100 RUB</g:price>' . PHP_EOL;
+        $result .= '<g:brand>Test</g:brand>' . PHP_EOL;
+        $result .= '<g:mpn>4687DF4</g:mpn>' . PHP_EOL;
+        $result .= '</item>' . PHP_EOL;
+        $result .= '</channel>' . PHP_EOL;
+        $result .= '</rss>' . PHP_EOL;
+
         return array(
             array ($feed, $result)
         );
